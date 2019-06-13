af248428f1cc32193609298d44be6ec05c1b14a5
arslanjavaid <arslan.leo@live.com>
2019-06-13 21:05:08 +0500

    Adding door animation based on door open/close.

--- js/door.js
+++ js/door.js
@@ -5,6 +5,13 @@ function Door() {
 
 Door.prototype.init = function() {
 
+    // check status
+    this.doorStatus();
+
+    // Check Door status
+    let self = this;
+    setInterval(function(){ self.doorStatus(); }, 3000);
+
     let ctx = document.getElementById('canvas').getContext('2d');
     let myChart = new Chart(ctx, {
         type: 'bar',
@@ -45,4 +52,46 @@ Door.prototype.openDoor = function (field) {
         $(".thumb").removeClass("thumbOpened");
         y.addClass("thumbOpened");
     }
+};
+
+
+
+Door.prototype.doorStatus = function () {
+    let $doorSelect = $('#doors-select'),
+        $door =  $(".thumb"),
+        fields = { DevEUI: $doorSelect.val() };
+
+    // Loading
+    $('.loading').show();
+
+    $.ajax({
+        url: 'https://twms75ak6c.execute-api.us-east-1.amazonaws.com/dev/door_status',
+        type: 'post',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data) {
+            console.log(data);
+            if(data.doorStatus == 'Door Open'){
+                $door.removeClass("thumbOpened");
+                $door.addClass("thumbOpened");
+            } else {
+                $door.removeClass("thumbOpened");
+            }
+        },
+        data: JSON.stringify(fields)
+    });
+
+    // $.post("https://twms75ak6c.execute-api.us-east-1.amazonaws.com/dev/contact", fields, function(response, status){
+    // $.post("http://localhost:3000/contact", fields, function(response, status){
+    //     let data = response.data;
+    //
+    //     console.log(data);
+    // }).fail(function(jqXHR, status, error) {
+    //     console.log( "error" );
+    // })
+    //     .always(function() {
+    //         // Loading
+    //         $('.loading').hide();
+    //         console.log( "finished" );
+    //     });
 };
