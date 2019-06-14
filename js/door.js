3748801d514a65480f876e0f097bbd8a017c6db7
usman2019 <uumar@macrosoftinc.com>
2019-06-14 17:21:24 +0500

    dynamic charts and door status

--- js/door.js
+++ js/door.js
@@ -4,42 +4,27 @@ function Door() {
 }
 
 Door.prototype.init = function() {
-
+    let labArray=['05-21', '05-22', '05-23', '05-24', '05-26', '05-27'];
+    let dataArray=[12, 19, 3, 5, 2, 3];
     // check status
     this.doorStatus();
 
     // Check Door status
     let self = this;
+    self.drawChart(labArray,dataArray);
+
     setInterval(function(){ self.doorStatus(); }, 3000);
 
-    let ctx = document.getElementById('canvas').getContext('2d');
-    let myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ['05-21', '05-22', '05-23', '05-24', '05-26', '05-27'],
-            datasets: [{
-                label: 'Door Open',
-                backgroundColor: 'rgba(54, 162, 235, 1)',
-                stack: 'Stack 0',
-                data: [12, 19, 3, 5, 2, 3],
-            }, {
-                label: 'Door Close',
-                backgroundColor: 'rgb(255, 99, 132)',
-                stack: 'Stack 1',
-                data: [12, 19, 3, 5, 2, 3],
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
+    self.chartData();
+
+    // Events
+    $('#door').on('change', function(){
+        // check status
+        self.doorStatus();
+        // Charts
+        self.chartData();
     });
-};
+};    
 
 Door.prototype.openDoor = function (field) {
     let y = $(field).find(".thumb"),
@@ -54,20 +39,81 @@ Door.prototype.openDoor = function (field) {
     }
 };
 
+Door.prototype.chartData = function () {
+   
+    let $doorSelect = $('#doors-select'), 
+    fields = { DevEUI: $doorSelect.val() };
+    let self = this;
+    let dataArray=[];
 
+    // Loading
+    $('.loading').show();
+    $.ajax({
+        url: 'https://twms75ak6c.execute-api.us-east-1.amazonaws.com/dev/contact',
+        type: 'post',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify(fields),
+
+        success: function (data) {
+            console.log(data);
+            // Loading
+            $('.loading').hide();
+            if(data.status){
+                console.log(Object.keys(data.series));
+                for (x in data.series){
+                    dataArray.push(data.series[x])
+                }
+           
+                self.drawChart(Object.keys(data.series),dataArray);
+
+            }
+        
+        },
+    });
+
+} 
+
+Door.prototype.drawChart=function(labArray,dataArray){
+    let ctx = document.getElementById('canvas').getContext('2d');
+    let myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labArray,
+            datasets: [{
+                label: 'Door Open',
+                backgroundColor: 'rgba(54, 162, 235, 1)',
+                stack: 'Stack 0',
+                data: dataArray,
+            }, {
+                label: 'Door Close',
+                backgroundColor: 'rgb(255, 99, 132)',
+                stack: 'Stack 1',
+                data: dataArray,
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
 
 Door.prototype.doorStatus = function () {
     let $doorSelect = $('#doors-select'),
         $door =  $(".thumb"),
         fields = { DevEUI: $doorSelect.val() };
 
-    // Loading
-    $('.loading').show();
-
     $.ajax({
         url: 'https://twms75ak6c.execute-api.us-east-1.amazonaws.com/dev/door_status',
         type: 'post',
         dataType: 'json',
+    
         contentType: 'application/json',
         success: function (data) {
             console.log(data);
@@ -81,6 +127,8 @@ Door.prototype.doorStatus = function () {
         data: JSON.stringify(fields)
     });
 
+    
+      
     // $.post("https://twms75ak6c.execute-api.us-east-1.amazonaws.com/dev/contact", fields, function(response, status){
     // $.post("http://localhost:3000/contact", fields, function(response, status){
     //     let data = response.data;
